622a5941eae7864cc7125791f2c455175730d882
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2022-12-23 20:43:45 -0800

    Condense all stats into tuple self.stats

--- character.py
+++ character.py
@@ -1,11 +1,14 @@
 import challenges
 
 
+def add_numbers(first, second):
+    return first + second
+
+
 class Character:
     """
     A class representing the character.
     """
-
     id_counter = 0
 
     def __init__(self, name):
@@ -14,10 +17,7 @@ class Character:
         else:
             self.name = name
 
-        self.charisma = 10
-        self.uniqueness = 10
-        self.nerve = 10
-        self.talent = 10
+        self.stats = (10, 10, 10, 10)
         self.location = 'werk_room'
         self.coordinates = (0, 4)
 
@@ -27,17 +27,8 @@ class Character:
     def get_name(self):
         return self.name
 
-    def get_charisma(self):
-        return self.charisma
-
-    def get_uniqueness(self):
-        return self.uniqueness
-
-    def get_nerve(self):
-        return self.nerve
-
-    def get_talent(self):
-        return self.nerve
+    def get_stats(self):
+        return self.stats
 
     def get_location(self):
         return self.location
@@ -50,24 +41,13 @@ class Character:
         Add base stats per base character type selection by user.
         """
         queen_types = ['Look Queen', 'Comedy Queen', 'Performance Queen', 'Alternative Queen']
-
-        queen_add_ons = ((4, 5, 0, 0), (5, 4, 0, 0), (7, 2, 0, 0), (2, 7, 0, 0))
+        stat_add_ons = ((4, 5, 0, 0), (5, 4, 0, 0), (7, 2, 0, 0), (2, 7, 0, 0))
 
         print("What type of queen are you?")
         answer = challenges.get_challenge_input_from_user(queen_types)
-        if answer == 'look_queen':
-            selected_add_ons = queen_add_ons[0]
-        elif answer == 'comedy_queen':
-            selected_add_ons = queen_add_ons[1]
-        elif answer == 'performance_queen':
-            selected_add_ons = queen_add_ons[2]
-        else:
-            selected_add_ons = queen_add_ons[3]
+        index = queen_types.index(answer)
 
-        self.charisma += selected_add_ons[0]
-        self.uniqueness += selected_add_ons[1]
-        self.nerve += selected_add_ons[2]
-        self.talent += selected_add_ons[3]
+        self.stats = tuple(map(add_numbers, self.stats, stat_add_ons[index]))
 
     def change_location(self, new_location):
         """
@@ -80,13 +60,10 @@ class Character:
         Update and print changes to stats for either the character.
         """
         stat_names = ['charisma', 'uniqueness', 'nerve', 'talent']
-        initial_stats = [self.charisma, self.uniqueness, self.nerve, self.talent]
+        initial_stats = self.stats
         index = 0
 
-        self.charisma += stat_changes[0]
-        self.uniqueness += stat_changes[1]
-        self.nerve += stat_changes[2]
-        self.talent += stat_changes[3]
+        self.stats = tuple(map(add_numbers, self.stats, stat_changes))
 
         for number in stat_changes:
             if number < 0:
@@ -101,10 +78,11 @@ class Character:
 def main():
     my_character = Character('Kelly')
     print(my_character.get_name())
-    print(my_character.get_uniqueness())
+    print(my_character.get_stats())
     print(my_character.get_location())
     print(my_character.get_coordinates())
     my_character.setup_character()
+    print(my_character.get_stats())
     my_character.power_up_or_down((1, 2, 0, 4))
 
 
