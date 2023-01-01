2d6d71327cea58762c8634738bf2ee8944521dba
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2022-12-31 21:03:50 -0800

    Update unit tests to test output

--- unit_testing/test_show_score.py
+++ unit_testing/test_show_score.py
@@ -1,20 +1,43 @@
-from unittest import TestCase
+import io
 from controls import show_score
+from unittest import TestCase
+from unittest.mock import patch
+import character
 
 
 class TestShowScore(TestCase):
-    def test_stats_all_negative_values(self):
-        self.assertEqual(
-            None, show_score({'Charisma': -5, 'Uniqueness': -25, 'Nerve': -400, 'Talent': -8}))
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_stats_all_negative_values(self, mock_output):
+        player = character.Character('Name')
+        player.set_stats([-5, -15, -400, -80])
+        show_score(player)
+        actual = mock_output.getvalue()
+        expected = 'Stats: [Charisma: -5, Uniqueness: -15, Nerve: -400, Talent: -80]\n'
+        self.assertEqual(expected, actual)
 
-    def test_stats_all_zero_values(self):
-        self.assertEqual(None,
-                         show_score({'Charisma': 0, 'Uniqueness': 0, 'Nerve': 0, 'Talent': 0}))
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_stats_all_zero_values(self, mock_output):
+        player = character.Character('Name')
+        player.set_stats([0, 0, 0, 0])
+        show_score(player)
+        actual = mock_output.getvalue()
+        expected = 'Stats: [Charisma: 0, Uniqueness: 0, Nerve: 0, Talent: 0]\n'
+        self.assertEqual(expected, actual)
 
-    def test_stats_all_positive_values(self):
-        self.assertEqual(None,
-                         show_score({'Charisma': 5, 'Uniqueness': 25, 'Nerve': 400, 'Talent': 8}))
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_stats_all_positive_values(self, mock_output):
+        player = character.Character('Name')
+        player.set_stats([50, 45, 9, 11])
+        show_score(player)
+        actual = mock_output.getvalue()
+        expected = 'Stats: [Charisma: 50, Uniqueness: 45, Nerve: 9, Talent: 11]\n'
+        self.assertEqual(expected, actual)
 
-    def test_stats_mix_of_values(self):
-        self.assertEqual(None,
-                         show_score({'Charisma': -5, 'Uniqueness': -25, 'Nerve': 500, 'Talent': 0}))
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_stats_mix_of_values(self, mock_output):
+        player = character.Character('Name')
+        player.set_stats([-16, 0, 4, 8])
+        show_score(player)
+        actual = mock_output.getvalue()
+        expected = 'Stats: [Charisma: -16, Uniqueness: 0, Nerve: 4, Talent: 8]\n'
+        self.assertEqual(expected, actual)
