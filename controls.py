2747d00dad0a661a1925bd48d86db441ddc67b94
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2022-12-31 18:41:13 -0800

    Add move_to_coordinates

--- controls.py
+++ controls.py
@@ -103,22 +103,22 @@ def get_directional_input_from_user(game_input: list, player) -> str:
     answer = input()
 
     if answer == '0':
-        show_score(character)
-        return get_directional_input_from_user(game_input, character)
+        show_score(player)
+        return get_directional_input_from_user(game_input, player)
     elif answer == 'quit':
         raise SystemExit("You quit the game.")
     elif answer not in acceptable_answers:
         print('That is not an acceptable answer! Please try again:')
-        return get_directional_input_from_user(game_input, character)
+        return get_directional_input_from_user(game_input, player)
     elif answer == 'X':
         print('Where the hell do you think you\'re going girl? Get your ass back in here!')
-        return get_directional_input_from_user(game_input, character)
-    elif answer == 'E' and character['location'] != 'judges_panel':
+        return get_directional_input_from_user(game_input, player)
+    elif answer == 'E' and player.get_location() != 'judges_panel':
         print('Not until you level up girl.')
-        return get_directional_input_from_user(game_input, character)
-    elif answer == 'Q' and character['location'] == 'main_stage':
+        return get_directional_input_from_user(game_input, player)
+    elif answer == 'Q' and player.get_location() == 'main_stage':
         print('What the hell are you doing girl? Get your ass to the other side of the stage!')
-        return get_directional_input_from_user(game_input, character)
+        return get_directional_input_from_user(game_input, player)
 
     answer_index = acceptable_answers.index(answer)
     answer_string = game_input[answer_index][1]
@@ -139,45 +139,41 @@ def move_character(player):
     board_name = player.get_location()
 
     game_input = generate_directional_inputs(current_coordinates, board_name)
-    move_to_coordinates = get_directional_input_from_user(game_input, character)
+    new_coordinates = get_directional_input_from_user(game_input, player)
 
-    if move_to_coordinates == 'enter':
-        if character['location'] == 'judges_panel':
-            character['met_rupaul'] = True
-            return boards.set_board(character)
-    elif move_to_coordinates == 'up':
-        move_to_coordinates = (current_coordinates[0] - 1, current_coordinates[1])
-    elif move_to_coordinates == 'down':
-        move_to_coordinates = (current_coordinates[0] + 1, current_coordinates[1])
-    elif move_to_coordinates == 'left':
-        move_to_coordinates = (current_coordinates[0], current_coordinates[1] - 1)
-    elif move_to_coordinates == 'right':
-        move_to_coordinates = (current_coordinates[0], current_coordinates[1] + 1)
-    elif move_to_coordinates == 'challenge her' and character['location'] == 'werk_room':
-        return challenges.makeover_challenge(character)
-    elif move_to_coordinates == 'challenge her' and character['location'] == 'dressing_room':
-        challenges.final_battle(character)
-        return character
+    if new_coordinates == 'enter':
+        if board_name == 'judges_panel':
+            return player.change_location('dressing_room')
+    elif new_coordinates == 'up':
+        new_coordinates = (current_coordinates[0] - 1, current_coordinates[1])
+    elif new_coordinates == 'down':
+        new_coordinates = (current_coordinates[0] + 1, current_coordinates[1])
+    elif new_coordinates == 'left':
+        new_coordinates = (current_coordinates[0], current_coordinates[1] - 1)
+    elif new_coordinates == 'right':
+        new_coordinates = (current_coordinates[0], current_coordinates[1] + 1)
+    elif new_coordinates == 'challenge her' and board_name == 'werk_room':
+        return challenges.makeover_challenge(player)
+    elif new_coordinates == 'challenge her' and board_name == 'dressing_room':
+        challenges.final_battle(player)
+        return player
     else:
-        move_to_coordinates = current_coordinates
+        new_coordinates = current_coordinates
 
-    character.update({'coordinates': move_to_coordinates})
-    return character
+    player.move_to_coordinates(new_coordinates)
+    return player
 
 
 def main():
     """
     Drive the program
     """
-    character = {'Charisma': 15, 'Uniqueness': 14, 'Nerve': 10, 'Talent': 10, 'met_rupaul': False,
-                 'completed_lip_sync': False, 'level': 1, 'Name': 'Ginger Snaps',
-                 'coordinates': (6, 3), 'location': 'main_stage'}
-    show_score(character)
-    print(f'Here is a demonstration of how user input is processed using move_character which uses '
-          f'generate_directional_inputs and get_directional_input_from_user to move the player '
-          f'character.')
-    move_character(character)
-    move_character(character)
+
+    # print(f'Here is a demonstration of how user input is processed using move_character which uses '
+    #       f'generate_directional_inputs and get_directional_input_from_user to move the player '
+    #       f'character.')
+    # move_character(player)
+    # move_character(player)
 
 
 if __name__ == '__main__':
